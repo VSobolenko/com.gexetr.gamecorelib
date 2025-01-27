d849f9c27bf24272cd2ecaafd71950cc0c7e18d1
Vladislav Sobolenko <sobolenko.alexplay@gmail.com>
2025-01-27 14:36:41 +0300

    drag and dprop feature and crud style buttons

--- Editor/EditorHelpers/SceneHelpers/SceneSwitcherEditorTool.cs
+++ Editor/EditorHelpers/SceneHelpers/SceneSwitcherEditorTool.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game;
 using Game.DynamicData;
 using UnityEditor;
 using UnityEditor.SceneManagement;
@@ -9,8 +12,7 @@ namespace GameEditor.SceneTools
 internal class SceneSwitcher : EditorWindow
 {
     private const string HeaderName = "Scene Loader";
-
-    private EditorBuildSettingsScene[] _scenes;
+    private const float ToolButtonWidth = 20f;
 
     [MenuItem(GameData.EditorName + EditorSubfolder.Scene + "/" + HeaderName)]
     private static void OpenWindow() => ShowWindow<SceneSwitcher>();
@@ -28,19 +30,20 @@ internal class SceneSwitcher : EditorWindow
 
     private void OnGUI()
     {
-        UpdateSceneCollection();
         GUILayout.BeginVertical();
         DrawSceneLoaderButtons();
         DrawStartedSceneSelection();
         GUILayout.EndVertical();
+        CollectDragAndDropScenes();
     }
 
-    private void DrawSceneLoaderButtons()
+    private static void DrawSceneLoaderButtons()
     {
-        if (_scenes.Length == 0)
+        if (EditorBuildSettings.scenes.Length == 0)
             GUILayout.Label("No Scenes in Build Settings");
-        foreach (var scene in _scenes)
+        foreach (var scene in EditorBuildSettings.scenes)
         {
+            GUILayout.BeginHorizontal();
             var sceneName = GetSceneNameWithoutExtension(scene);
             GUI.color = !scene.enabled ? new Color(0.75f, 0.75f, 0.75f) : Color.white;
             if (GUILayout.Button($"Load {sceneName}"))
@@ -49,7 +52,17 @@ internal class SceneSwitcher : EditorWindow
                 GUIUtility.ExitGUI();
             }
 
+            var sceneEnableToggle = GUILayout.Toggle(scene.enabled, "", GUILayout.Width(ToolButtonWidth)); // Enable ✓
+            ToggleSceneEnabled(scene, sceneEnableToggle);
+            if (GUILayout.Button($"\u2193", GUILayout.Width(ToolButtonWidth))) // Up ↑
+                MoveSceneToShiftIndex(scene, 1);
+            if (GUILayout.Button($"\u2191", GUILayout.Width(ToolButtonWidth))) // Down ↓
+                MoveSceneToShiftIndex(scene, -1);
+            if (GUILayout.Button($"\u00d7", GUILayout.Width(ToolButtonWidth))) // Remove ×
+                RemoveSceneFromBuildSettings(scene);
+
             GUI.color = Color.white;
+            GUILayout.EndHorizontal();
         }
     }
 
@@ -129,14 +142,105 @@ internal class SceneSwitcher : EditorWindow
         return possibleScenesToLoad;
     }
 
-    private void UpdateSceneCollection() => _scenes = EditorBuildSettings.scenes;
-
     private static void OpenScene(EditorBuildSettingsScene scene)
     {
         if (EditorApplication.isPlaying)
             return;
         if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
             EditorSceneManager.OpenScene(scene.path);
+    }
+
+    private static void ToggleSceneEnabled(EditorBuildSettingsScene scene, bool sceneEnableToggle)
+    {
+        if (sceneEnableToggle == scene.enabled)
+            return;
+
+        var index = FindIndex(scene);
+        var sceneArray = EditorBuildSettings.scenes;
+        sceneArray[index].enabled = sceneEnableToggle;
+        EditorBuildSettings.scenes = sceneArray;
+    }
+
+    private static void MoveSceneToShiftIndex(EditorBuildSettingsScene scene, int n)
+    {
+        var index = FindIndex(scene);
+        if (index < 0 || index >= EditorBuildSettings.scenes.Length)
+            return;
+
+        var newIndex = index + n;
+        if (newIndex < 0 || newIndex >= EditorBuildSettings.scenes.Length)
+            return;
+
+        var sceneArray = EditorBuildSettings.scenes;
+        if (newIndex > index)
+            for (var i = index; i < newIndex; i++)
+                sceneArray[i] = sceneArray[i + 1];
+        else
+            for (var i = index; i > newIndex; i--)
+                sceneArray[i] = sceneArray[i - 1];
+
+        sceneArray[newIndex] = scene;
+        EditorBuildSettings.scenes = sceneArray;
+    }
+
+    private static void RemoveSceneFromBuildSettings(EditorBuildSettingsScene scene) => EditorBuildSettings.scenes =
+        EditorBuildSettings.scenes.ToList().Where(x => x.guid != scene.guid).ToArray();
+
+    private void CollectDragAndDropScenes()
+    {
+        var fullArea = new Rect(0, 0, position.width, position.height);
+        var evt = Event.current;
+
+        switch (evt.type)
+        {
+            case EventType.DragUpdated:
+            case EventType.DragPerform:
+                if (fullArea.Contains(evt.mousePosition))
+                {
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                    if (evt.type == EventType.DragPerform)
+                    {
+                        DragAndDrop.AcceptDrag();
+
+                        foreach (var draggedObject in DragAndDrop.objectReferences)
+                        {
+                            var path = AssetDatabase.GetAssetPath(draggedObject);
+
+                            if (path.EndsWith(".unity"))
+                                AddSceneToBuildSettings(path);
+                            else
+                                Log.Warning("The object is not a scene: " + path);
+                        }
+
+                        evt.Use();
+                    }
+                }
+
+                break;
+        }
+    }
+
+    private static void AddSceneToBuildSettings(string scenePath)
+    {
+        var scenesInBuildSettings = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
+        var sceneAlreadyAdded = scenesInBuildSettings.Exists(scene => scene.path == scenePath);
+
+        if (!sceneAlreadyAdded)
+        {
+            scenesInBuildSettings.Add(new EditorBuildSettingsScene(scenePath, true));
+            EditorBuildSettings.scenes = scenesInBuildSettings.ToArray();
+        }
+        else
+            Log.Warning("Scene is already in Build Settings: " + scenePath);
+    }
+
+    private static int FindIndex(EditorBuildSettingsScene obj)
+    {
+        for (var i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            if (EditorBuildSettings.scenes[i].guid == obj.guid)
+                return i;
+        return -1;
     }
 }
 }
