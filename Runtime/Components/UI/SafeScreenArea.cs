a36133634243017e085802638f2bf96cc06e7487
VSobolenko <v.sobolenko.vs@gmail.com>
2025-05-15 14:07:23 +0300

    update safe screen mode

--- Runtime/Components/UI/SafeScreenArea.cs
+++ Runtime/Components/UI/SafeScreenArea.cs
@@ -3,30 +3,58 @@ using UnityEngine;
 
 namespace Game.Components
 {
+[System.Flags]
+public enum SafeAreaEdge
+{
+    None   = 0,
+    Left   = 1 << 0,
+    Right  = 1 << 1,
+    Top    = 1 << 2,
+    Bottom = 1 << 3,
+    All    = Left | Right | Top | Bottom
+}
+
 [DisallowMultipleComponent]
 [RequireComponent(typeof(RectTransform))]
-internal class SafeScreenArea : MonoBehaviour
+public class SafeScreenArea : MonoBehaviour
 {
-    [SerializeField, HideInInspector] private RectTransform rectTransformComponent;
+    [SerializeField] private RectTransform rectTransformComponent;
+    [SerializeField] private SafeAreaEdge initialEdge = SafeAreaEdge.All;
+    [SerializeField] private bool initialEnable = true;
+
+    public RectTransform RectTransform => rectTransformComponent;
     
     private void Awake()
     {
-        RecalculateRectTransform();
+        if (initialEnable)
+            RecalculateRectTransform(initialEdge);
     }
 
-    public void RecalculateRectTransform()
+    public void RecalculateRectTransform(SafeAreaEdge edges)
     {
         var safeArea = Screen.safeArea;
-        var anchorMin = safeArea.position;
-        var anchorMax = anchorMin + safeArea.size;
-        anchorMin.x /= Screen.width;
-        anchorMin.y /= Screen.height;
-        anchorMax.x /= Screen.width;
-        anchorMax.y /= Screen.height;
+
+        var minX = safeArea.x / Screen.width;
+        var maxX = (safeArea.x + safeArea.width) / Screen.width;
+        var minY = safeArea.y / Screen.height;
+        var maxY = (safeArea.y + safeArea.height) / Screen.height;
+
+        var anchorMin = rectTransformComponent.anchorMin;
+        var anchorMax = rectTransformComponent.anchorMax;
+
+        if ((edges & SafeAreaEdge.Left) != 0)
+            anchorMin.x = minX;
+        if ((edges & SafeAreaEdge.Right) != 0)
+            anchorMax.x = maxX;
+        if ((edges & SafeAreaEdge.Bottom) != 0)
+            anchorMin.y = minY;
+        if ((edges & SafeAreaEdge.Top) != 0)
+            anchorMax.y = maxY;
+
         rectTransformComponent.anchorMin = anchorMin;
         rectTransformComponent.anchorMax = anchorMax;
     }
-     
+    
     [ContextMenu("Force Reset")]
     private void Reset() => this.With(x => x.rectTransformComponent = GetComponent<RectTransform>(), rectTransformComponent == null);
 }
